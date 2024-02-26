ff8854e4c8fea0069dde57427af54fc3851b8b25
irak1kvinchia <kvinchiairakli3@gmail.com>
2024-02-26 21:16:57 +0400

    commit

---
+++ Day 07/homeworks/lesson7.py
@@ -0,0 +1,16 @@
+# for i in range(0,10,2):
+#     print(i)
+
+# counter = 0
+# while counter < 20:
+#     print(counter)
+#     counter += 1
+
+#sum = 0
+#for i in range(6):
+#    sum += i
+#print(sum)
+
+# text = "harry kote"
+# for i in text:
+#     print(i)
